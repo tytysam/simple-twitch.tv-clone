cade7e8db498feed6c710edd310c183a69f0a155
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-03-05 13:12:42 -0700

    Imported Semantic-UI + built a simple Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
-import StreamCreate from './streams/StreamCreate.js'
-import StreamDestroy from './streams/StreamDestroy.js'
-import StreamEdit from './streams/StreamEdit.js'
-import StreamList from './streams/StreamList.js'
-import StreamShow from './streams/StreamShow.js'
-
-
-
+import Header from "./Header.js";
+import StreamCreate from "./streams/StreamCreate.js";
+import StreamDestroy from "./streams/StreamDestroy.js";
+import StreamEdit from "./streams/StreamEdit.js";
+import StreamList from "./streams/StreamList.js";
+import StreamShow from "./streams/StreamShow.js";
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
+          <Header />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/edit" exact component={StreamEdit} />
@@ -23,7 +22,7 @@ const App = () => {
         </div>
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
 export default App;
