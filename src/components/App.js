aff653cfc6c546cd3dca021fc67ebd866a24a3d7
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-03-05 12:44:38 -0700

    Initial commit. Removed unneeded files, scaffolded App.js and index.js

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+
+
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>APP</div>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App;
