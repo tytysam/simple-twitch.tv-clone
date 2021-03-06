4cd8f01eae294e39378a8b53502d4b6279082674
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-03-06 14:10:51 -0700

    Working Auth with Google OAuth. Currently renders login state

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,20 +1,37 @@
 import React, { Component } from "react";
 
 class GoogleAuth extends Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "697201807028-r90elb7uil79cr9980ctk25sb4cebubs.apps.googleusercontent.com",
-        scope: "email",
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "697201807028-r90elb7uil79cr9980ctk25sb4cebubs.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div> I don't know if we are signed in...</div>;
+    } else if (this.state.isSignedIn) {
+      return <div> I am signed in! </div>;
+    } else {
+      return <div>I am not signed in :( </div>;
+    }
   }
 
   render() {
     return (
       <div>
-        <div>Google Auth</div>
+        <div>{this.renderAuthButton()}</div>
       </div>
     );
   }
