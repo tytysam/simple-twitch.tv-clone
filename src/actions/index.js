4dfdb01d70d452acd06d2cffee1fb351760377be
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-03-08 13:33:46 -0700

    Built the rest of my Action Creators in-bulk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from "../apis/streams.js";
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./types.js";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from "./types.js";
 
 export const signIn = (userId) => {
   return {
@@ -17,5 +25,31 @@ export const signOut = () => {
 export const createStream = (formValues) => {
   return async (dispatch) => {
     const res = await streams.post("/streams", formValues);
+
+    dispatch({ type: CREATE_STREAM, payload: res.data });
   };
+};
+
+export const fetchStreams = () => async (dispatch) => {
+  const res = await streams.get("/streams");
+
+  dispatch({ type: FETCH_STREAMS, payload: res.data });
+};
+
+export const fetchStream = (id) => async (dispatch) => {
+  const res = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: res.data });
+};
+
+export const editStream = (id, formValues) => async (dispatch) => {
+  const res = await streams.put(`/streams/${id}`, formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: res.data });
+};
+
+export const deleteStream = (id) => async (dispatch) => {
+  const res = await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
 };
