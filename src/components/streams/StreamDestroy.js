4247efe51cf66c518902cdce57d1edd93511b7a5
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-03-09 11:09:37 -0700

    Refactored Modal to be reusable across components. Inherits content via props

--- src/components/streams/StreamDestroy.js
+++ src/components/streams/StreamDestroy.js
@@ -2,10 +2,21 @@ import React from "react";
 import Modal from "../Modal.js";
 
 const StreamDestroy = () => {
+  const actions = (
+    <div>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button ">Cancel</button>
+    </div>
+  );
+
   return (
     <div>
       <div>StreamDestroy</div>
-      <Modal />
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+      />
     </div>
   );
 };
