6f84329586753aab6a0de28cec82db22c2118030
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-03-09 11:16:21 -0700

    Also added the onClick of the root Modal div as an inheritable prop controlled by the parent. Modal should be entirely reusable now!

--- src/components/streams/StreamDestroy.js
+++ src/components/streams/StreamDestroy.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Modal from "../Modal.js";
+import history from "../../history.js";
 
 const StreamDestroy = () => {
   const actions = (
@@ -10,14 +11,14 @@ const StreamDestroy = () => {
   );
 
   return (
-    <div>
-      <div>StreamDestroy</div>
+    <>
       <Modal
         title="Delete Stream"
         content="Are you sure you want to delete this stream?"
         actions={actions}
+        onDismiss={() => history.push("/")}
       />
-    </div>
+    </>
   );
 };
 
